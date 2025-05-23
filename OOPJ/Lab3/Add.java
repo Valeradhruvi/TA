7b1d2efa61015994e62c52fb66b8bd30fa5ff7fc
Valeradhruvi <140879302+Valeradhruvi@users.noreply.github.com>
2025-05-23 11:34:39 +0530

    Add files via upload
    TA-OOPJ-Lab3

---
+++ OOPJ/Lab3/Add.java
@@ -0,0 +1,10 @@
+public class Add {
+    public static void main(String[] args) {
+        System.out.println("Darshan University,");
+        System.out.println("Hadala,");
+        System.out.println("Rajkot");
+
+        //single line
+        System.out.println("Darshan university, \n Hadala, \n Rajkot");
+    }
+}
